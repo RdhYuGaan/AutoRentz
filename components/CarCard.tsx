7ce9ef92949d4c58d4815bcb2f2733812596ec29
RdhYuGaan <radithyugan22@gmail.com>
2024-10-03 19:14:45 +0530

    add carimage page

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,8 +3,9 @@ import Image from 'next/image';
 import {useState} from 'react'
 import {CarProps} from '../types';
 import CustomButton from './CustomButton';
-import { calculateCarRent } from '@/utils';
+import { calculateCarRent, generateCarImageUrl } from '@/utils';
 import CarDetails from './CarDetails';
+
 
 interface CarCardProps{
     car:CarProps;
@@ -34,7 +35,8 @@ const CarCard = ({car}: CarCardProps) => {
             </span>
         </p>
         <div className='relative w-full h-40 my-3 object-contain'>
-            <Image src='/hero.png' alt='car model' fill priority className='object-contain' />
+            <Image src={generateCarImageUrl(car)} alt='car model' 
+            fill priority className='object-contain' />
         </div>
 
         <div className='relative flex w-full mt-2'>
--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -3,6 +3,8 @@ import React, { Fragment } from 'react';
 import { CarProps } from '@/types';
 import  Image from 'next/image';
 import { Dialog,Transition } from '@headlessui/react';
+import { generateCarImageUrl } from '@/utils';
+import { url } from 'inspector';
 
 interface CarDetailsProps {
  isOpen:boolean;
@@ -50,7 +52,8 @@ const CarDetails = ({ isOpen,classModel, car}:
           >
             <Dialog.Panel className="relative w-full max-w-lg
             max-h[90vh] overflow-y-auto transform rounded-2xl
-             bg-white text-left shadow-xsl transition-all flex flex-col gap-5">
+             bg-white p-8 text-left shadow-xl transition-all 
+             flex flex-col gap-5">
               <button
                 type='button'
                 onClick={closeModal}
@@ -73,12 +76,48 @@ const CarDetails = ({ isOpen,classModel, car}:
                 <div className='flex-1 flex flex-col gap-3'>
                   <div className='relative w-full h-40 bg-pattern bg-cover
                    bg-center rounded-lg'>
-                     <Image src='/hero.png' alt='car model' fill priority 
+                     <Image src={generateCarImageUrl(car,'angle')} alt='car model' fill priority 
                      className='object-contain' />
+                
+                  </div>
 
-
+                  <div className='flex gap-3'>
+                    <div className='flex-1 relative w-full h-24
+                     bg-primary-blue-100 rounded-lg'>
+                      <Image src={generateCarImageUrl(car,'angle')} alt="car model" 
+                      fill priority className='object-contain '/>
+                    </div>
+                    <div className='flex-1 relative w-full h-24
+                     bg-primary-blue-100 rounded-lg'>
+                      <Image src={generateCarImageUrl(car,'angle')} alt="car model" 
+                      fill priority className='object-contain '/>
+                    </div>
+                    <div className='flex-1 relative w-full h-24
+                     bg-primary-blue-100 rounded-lg'>
+                      <Image src={generateCarImageUrl(car,'angle')} alt="car model" 
+                      fill priority className='object-contain '/>
+                    </div>
                   </div>
                 </div>
+
+                <div className="flex-1 flex-col flex gap-2">
+  <h2 className="font-semibold text-xl capitalize">
+    {car.make} {car.model}
+  </h2>
+
+  <div className="mt-3 flex flex-wrap gap-4">
+    
+    {Object.entries(car).map(([key, value]) => (
+      <div className="flex justify-between 
+      gap-5 w-full text-right" key={key}>
+        
+        <h4 className='text-gray capitalize'>{key.split("_").join(" ")}</h4>
+        <p className='font-semibold text-black'>{value}</p>
+      </div>
+    ))}
+  </div>
+</div>
+
             </Dialog.Panel>
 
           </Transition.Child>  
