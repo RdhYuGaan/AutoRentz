1d5a4d3fd012544b37f4fa647891e2d36a945315
RdhYuGaan <radithyugan22@gmail.com>
2024-10-09 01:30:53 +0530

    add some function

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -11,10 +11,19 @@ const CustomFilter = ({title, options}:
     const [selected,setSelected]= useState(options[0]);
   return (
     <div className='w-fit'>
-      <Listbox>
+      <Listbox value={selected} onChange={(e)=>setSelected(e)}>
         <div className='relative w-fit z-10'>
           <Listbox.Button className="custom-bilter__btn">
-            <span>Filter</span>
+            <span className='block truncate'>{selected.title}</span>
+            <Image 
+              src="/chevron-up-down.svg"
+              width={20}
+              height={20}
+              className='ml-4 object-contain'
+              alt="chevron up down"
+            />
+              
+            
           </Listbox.Button>
         </div>
       </Listbox>
